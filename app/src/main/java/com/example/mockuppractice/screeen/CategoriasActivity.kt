5729ce35e46559fe3c6fca001cc2e3979ba31945
irling <italodavidramosling@gmail.com>
2024-09-11 00:16:09 -0500

    Se añadio iconos de facebook y google

--- app/src/main/java/com/example/mockuppractice/screeen/CategoriasActivity.kt
+++ app/src/main/java/com/example/mockuppractice/screeen/CategoriasActivity.kt
@@ -1,10 +1,11 @@
-package com.example.mockuppractice
+package com.example.mockuppractice.screeen
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.mockuppractice.R
 
 class CategoriasActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
