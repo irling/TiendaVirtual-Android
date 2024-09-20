0eef77aeb2d0970d42f08e6835e38fbf45b80bfb
irling <italodavidramosling@gmail.com>
2024-09-20 16:46:50 -0500

    Se añadio la logica para guardar los datos

--- app/src/main/java/com/example/mockuppractice/screeen/Direcciones/AddDireccionActivity.kt
+++ app/src/main/java/com/example/mockuppractice/screeen/Direcciones/AddDireccionActivity.kt
@@ -5,6 +5,8 @@ import android.widget.Adapter
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -12,8 +14,13 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.mockuppractice.R
 import com.example.mockuppractice.screeen.CategoriasActivity
+import com.google.gson.Gson
 
 class AddDireccionActivity : CategoriasActivity() {
+
+    private val direcciones = mutableListOf<formDireccion>()
+    private val gson = Gson()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -23,5 +30,38 @@ class AddDireccionActivity : CategoriasActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        SaveDataForm()
+
     }
+
+    private fun SaveDataForm() {
+        val ETdireccion = findViewById<EditText>(R.id.edDireccion)
+        val ETnumeroDire = findViewById<EditText>(R.id.edNumeroCasa)
+        val ETtipoDomi = findViewById<EditText>(R.id.edTipo)
+        val ETreferencia = findViewById<EditText>(R.id.edreferencia)
+        val botonSave = findViewById<Button>(R.id.btnGuardarDireccion)
+
+        botonSave.setOnClickListener {
+            val direccion = ETdireccion.text.toString()
+            val numeroDire = ETnumeroDire.text.toString()
+            val tipoDomi = ETtipoDomi.text.toString()
+            val referencia = ETreferencia.text.toString()
+
+            val nuevaDireccion = formDireccion(direccion, numeroDire, tipoDomi, referencia)
+            direcciones.add(nuevaDireccion)
+
+            val sharedPreferences = getSharedPreferences("DireccionUsers", MODE_PRIVATE)
+            val editor = sharedPreferences.edit()
+            val json = gson.toJson(direcciones)
+            editor.putString("Direcciones", json)
+            editor.apply()
+
+            Toast.makeText(this, "Datos guardados exitosamente", Toast.LENGTH_SHORT).show()
+
+        }
+
+    }
+
+
 }
