d4bf16446e2913be2e30f02a7e325791fd5bca8a
irling <italodavidramosling@gmail.com>
2024-09-20 16:47:28 -0500

    Revisar si esta gucci la logica de mostrar datos guardados con gson

--- app/src/main/java/com/example/mockuppractice/screeen/Direcciones/ShowDireccionActivity.kt
+++ app/src/main/java/com/example/mockuppractice/screeen/Direcciones/ShowDireccionActivity.kt
@@ -1,13 +1,24 @@
 package com.example.mockuppractice.screeen.Direcciones
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.mockuppractice.R
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 
 class ShowDireccionActivity : AppCompatActivity() {
+
+    private val gson = Gson()
+
+    override fun onStart() {
+        super.onStart()
+        showDataDirecciones()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -18,4 +29,21 @@ class ShowDireccionActivity : AppCompatActivity() {
             insets
         }
     }
+
+    private fun showDataDirecciones() {
+        val tvShowDirec = findViewById<TextView>(R.id.tvShowDirecciones)
+
+        val sharedPreferences = getSharedPreferences("DireccionUsers", MODE_PRIVATE)
+        val json = sharedPreferences.getString("Direcciones", "[]")
+
+        val type = object : TypeToken<formDireccion>() {}.type
+        val direcciones: List<formDireccion> = gson.fromJson(json, type)
+
+        val direccionText = direcciones.joinToString("\n") {
+            "Dirección: ${it.direccion}\n Número: ${it.numeroDire}\n Tipo Domicilio: ${it.tipoDomi}\n Referencia: ${it.referencia}"
+        }
+
+        tvShowDirec.text = direccionText
+    }
+
 }
