d7af30feceacc044bece861e38afe1ae46d8c235
irling <italodavidramosling@gmail.com>
2024-09-20 16:48:15 -0500

    se añadio dataClass con los tipos de datos que se almacena en el formulario

--- app/src/main/java/com/example/mockuppractice/screeen/Direcciones/dataDireccion.kt
+++ app/src/main/java/com/example/mockuppractice/screeen/Direcciones/dataDireccion.kt
@@ -1,4 +1,8 @@
 package com.example.mockuppractice.screeen.Direcciones
 
-class dataDireccion {
-}
+data class formDireccion(
+    val direccion: String,
+    val numeroDire: String,
+    val tipoDomi: String,
+    val referencia: String
+)
