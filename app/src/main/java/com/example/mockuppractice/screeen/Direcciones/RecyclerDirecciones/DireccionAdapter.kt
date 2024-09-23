9b75d48221c371e74d3a6c469b90ff429bac8739
irling <italodavidramosling@gmail.com>
2024-09-23 07:49:10 -0500

    actualizacion de direccion, ya funcionando

---
+++ app/src/main/java/com/example/mockuppractice/screeen/Direcciones/RecyclerDirecciones/DireccionAdapter.kt
@@ -0,0 +1,57 @@
+package com.example.mockuppractice.screeen.Direcciones.RecyclerDirecciones
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.mockuppractice.R
+import com.example.mockuppractice.screeen.Direcciones.formDireccion
+
+class DireccionAdapter(
+    private val context: Context,
+    private val direcciones: MutableList<formDireccion>,
+    private val onEditClicked: (Int) -> Unit,
+    private val onDeleteClicked: (Int) -> Unit
+) : RecyclerView.Adapter<DireccionAdapter.DireccionViewHolder>() {
+
+    class DireccionViewHolder(View: View) : RecyclerView.ViewHolder(View){
+        val tvDireccion: TextView = itemView.findViewById(R.id.tvDireccion)
+        val tvNumeroCasa: TextView = itemView.findViewById(R.id.tvNumeroCasa)
+        val tvTipoDomi: TextView = itemView.findViewById(R.id.tvTipoDomi)
+        val tvReferencia: TextView = itemView.findViewById(R.id.tvReferencia)
+        val btnEdit: Button = itemView.findViewById(R.id.btnEdit)
+        val btnDelete: Button = itemView.findViewById(R.id.btnDelete)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DireccionViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.item_show_direccion, parent, false)
+        return DireccionViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: DireccionViewHolder, position: Int) {
+        val direccion = direcciones[position]
+
+        holder.tvDireccion.text = "Dirección: ${direccion.direccion}"
+        holder.tvNumeroCasa.text = "Número: ${direccion.numeroDire}"
+        holder.tvTipoDomi.text = "Tipo Domicilio: ${direccion.tipoDomi}"
+        holder.tvReferencia.text = "Referencia: ${direccion.referencia}"
+
+        // Botón de Editar
+        holder.btnEdit.setOnClickListener {
+            onEditClicked(position)
+        }
+
+        // Botón de Eliminar
+        holder.btnDelete.setOnClickListener {
+            onDeleteClicked(position)
+        }
+
+    }
+
+    override fun getItemCount(): Int = direcciones.size
+
+
+}
--- app/src/main/java/com/example/mockuppractice/screeen/Direcciones/ShowDireccionActivity.kt
+++ app/src/main/java/com/example/mockuppractice/screeen/Direcciones/ShowDireccionActivity.kt
@@ -1,12 +1,17 @@
 package com.example.mockuppractice.screeen.Direcciones
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.mockuppractice.R
+import com.example.mockuppractice.screeen.Direcciones.RecyclerDirecciones.DireccionAdapter
 import com.google.common.reflect.TypeToken
 import com.google.gson.Gson
 
@@ -14,10 +19,8 @@ class ShowDireccionActivity : AppCompatActivity() {
 
     private val gson = Gson()
 
-    override fun onStart() {
-        super.onStart()
-        showDataDirecciones()
-    }
+    private lateinit var direcciones: MutableList<formDireccion>
+    private lateinit var adapter: DireccionAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,19 +36,45 @@ class ShowDireccionActivity : AppCompatActivity() {
     }
 
     private fun showDataDirecciones() {
-        val tvShowDirec = findViewById<TextView>(R.id.tvShowDirecciones)
+        val recyclerView = findViewById<RecyclerView>(R.id.rvDirecciones)
+        recyclerView.layoutManager = LinearLayoutManager(this)
 
         val sharedPreferences = getSharedPreferences("DireccionUsers", MODE_PRIVATE)
         val json = sharedPreferences.getString("Direcciones", "[]")
 
         val type = object : TypeToken<List<formDireccion>>() {}.type
-        val direcciones: List<formDireccion> = gson.fromJson(json, type)
+        direcciones = gson.fromJson<MutableList<formDireccion>?>(json, type).toMutableList()
 
-        val direccionText = direcciones.joinToString("\n") {
-            "Dirección: ${it.direccion}\n Número: ${it.numeroDire}\n Tipo Domicilio: ${it.tipoDomi}\n Referencia: ${it.referencia}"
-        }
+        adapter = DireccionAdapter(
+            this,
+            direcciones,
+            onEditClicked = { position -> editDireccion(position) },
+            onDeleteClicked = { position -> deleteDireccion(position) }
 
-        tvShowDirec.text = direccionText
+        )
+        recyclerView.adapter = adapter
+    }
+
+    private fun deleteDireccion(position: Int) {
+        val intent = Intent(this, AddDireccionActivity::class.java)
+        intent.putExtra("direcciones", position)
+        startActivity(intent)
+    }
+
+    private fun editDireccion(position: Int) {
+        direcciones.removeAt(position)
+
+        //guarda las direccciones
+        val sharedPreferences = getSharedPreferences("direccionesUsers", MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        val jsonUpdate = gson.toJson(direcciones)
+        editor.putString("Direcciones", jsonUpdate)
+        editor.apply()
+
+        adapter.notifyItemRemoved(position)
+
+        Toast.makeText(this, "Direccion Eliminada Correctamente", Toast.LENGTH_SHORT).show()
+
     }
 
 }
