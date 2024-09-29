c64593efb87f8ca324d27b2e9c4be0f8531adaa7
irling <italodavidramosling@gmail.com>
2024-09-29 03:08:06 -0500

    se añadio inicio de sesion con google

--- app/src/main/java/com/example/mockuppractice/screeen/RecyclerViewShirt/PagesShirt/Shirt01.kt
+++ app/src/main/java/com/example/mockuppractice/screeen/RecyclerViewShirt/PagesShirt/Shirt01.kt
@@ -1,6 +1,10 @@
 package com.example.mockuppractice.screeen.RecyclerViewShirt.PagesShirt
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,6 +13,11 @@ import com.example.mockuppractice.R
 import com.google.android.gms.wallet.PaymentsClient
 
 class Shirt01 : AppCompatActivity() {
+
+    private lateinit var btnNext: ImageButton
+    private lateinit var btnBack: ImageButton
+
+    private lateinit var btnSave: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +29,24 @@ class Shirt01 : AppCompatActivity() {
             insets
         }
 
+        initComponents()
+        changePage()
 
+    }
+
+    private fun initComponents() {
+        btnNext = findViewById(R.id.ibtnDerecha)
+        btnBack = findViewById(R.id.ibtnIzquierda)
+        btnSave = findViewById(R.id.btnSave01)
+    }
+
+    private fun changePage() {
+        btnNext.setOnClickListener {
+            val intentNext = Intent(this, Shirt02::class.java)
+            startActivity(intentNext)
+        }
+        btnBack.setOnClickListener {
+            Toast.makeText(this, "No hay más productos", Toast.LENGTH_SHORT).show()
+        }
     }
 }
